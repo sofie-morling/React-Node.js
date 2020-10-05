62e494fa9d96d5d440ca0318ee2090a58e18d0bd
Sofie Morling <you@example.com>
2020-10-05 13:29:22 +0200

    add redux to homepage and new files to that

--- amazon/frontend/src/Screens/HomeScreen.js
+++ amazon/frontend/src/Screens/HomeScreen.js
@@ -1,23 +1,26 @@
 import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 import axios from 'axios';
+import { useDispatch, useSelector } from 'react-redux';
+import { listProducts } from '../actions/productActions';
 
 function HomeScreen (props) {
 
-    const [products, setProduct] = useState([]);
+    
+    const productList = useSelector(state => state.productList);
+    const { products, loading, error } = productList;
+    const dispatch = useDispatch();
 
     useEffect(() => {
-        const fetchData = async () =>{
-            const {data} = await axios.get("/api/products");
-            setProduct(data);
-        }
-        fetchData();
+        dispatch(listProducts());
         return () => {
             //
         };
     }, [])
     
-    return <ul className="products">
+    return loading ? <div>Loading...</div> :
+    error ? <div>{error}</div> :
+    <ul className="products">
     {
       products.map(product => 
         <li key={product._id}>
--- amazon/frontend/src/store.js
+++ amazon/frontend/src/store.js
@@ -1,5 +1,13 @@
-import {createStore, combineReducers} from 'redux';
+import {createStore, combineReducers, applyMiddleware, compose} from 'redux';
+import thunk from 'redux-thunk';
+import { productListReducer } from './reducers/productReducers';
 
 
 const initialState = {};
-const store = createStore(reducer, initialState);
+const reducer = combineReducers({
+    productList: productListReducer,
+})
+const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const store = createStore(reducer, initialState, composeEnhancer(applyMiddleware(thunk)));
+
+export default store;

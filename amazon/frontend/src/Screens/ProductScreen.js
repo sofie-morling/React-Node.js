7084005e35af106531d43211aa4ed4db208adc66
Sofie Morling <you@example.com>
2020-10-06 11:22:24 +0200

    add redux to detail products

--- amazon/frontend/src/Screens/ProductScreen.js
+++ amazon/frontend/src/Screens/ProductScreen.js
@@ -1,15 +1,33 @@
-import React from 'react';
-import data from '../data';
+import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
+import {useDispatch, useSelector} from 'react-redux';
+import { detailsProduct } from '../actions/productActions';
 
-function ProductScreen (props) {
-    console.log(props.match.params.id);
-    const product = data.products.find(x => x._id === props.match.params.id);
+function ProductScreen(props) {
+    const [qty, setQty] = useState(1);
+    const productDetails = useSelector(state => state.productDetails);
+    const { product, loading, error } = productDetails;
+    const dispatch = useDispatch();
+
+    useEffect (() => {
+        dispatch(detailsProduct(props.match.params.id));
+        return () => {
+            //
+        };
+    }, []);
+
+    const handleAddToCart = () => {
+        props.history.push("/cart/" + props.match.params.id + "?qty=" + qty)
+    }
+
     return <div>
         <div className="back-to-coctails">
             <Link to="/">Back to more Coctails</Link>
         </div>
-        <div className="details">
+        {loading? <div>Loading ..</div>:
+        error? <div>{error}</div>:
+        (
+            <div className="details">
             <div className="details-image">
               <img src={product.image} alt="product"></img>
             </div>
@@ -38,19 +56,20 @@ function ProductScreen (props) {
                         Status: {product.status}
                     </li>
                     <li>
-                        Qty; <select>
-                            <option>1</option>
-                            <option>2</option>
-                            <option>3</option>
-                            <option>4</option>
+                        Qty: <select value={qty} onChange={(e) => { setQty(e.target.value)}}>
+                           {[...Array(product.countInStock).keys()].map(x =>
+                            <option key={x + 1} value={x + 1}>{x + 1}</option>
+                            )}
                         </select>
                     </li>
                     <li>
-                        <button className="button">Add to basket</button>
+                        <button onClick={handleAddToCart} className="button">Add to basket</button>
                     </li>
                 </ul>
             </div>
         </div>
+        )
+        }
         
     </div>
 }
--- amazon/frontend/src/actions/productActions.js
+++ amazon/frontend/src/actions/productActions.js
@@ -1,4 +1,6 @@
-import { PRODUCT_LIST_FAIL, PRODUCT_LIST_REQUEST, PRODUCT_LIST_SUCCESS } from "../constants/productConstants";
+import { PRODUCT_LIST_FAIL, PRODUCT_LIST_REQUEST, PRODUCT_LIST_SUCCESS, 
+    PRODUCT_DETAILS_SUCCESS, PRODUCT_DETAILS_REQUEST, PRODUCT_DETAILS_FAIL } from "../constants/productConstants";
+
 import axios from 'axios';
 
 const listProducts = () => async (dispatch) => {
@@ -8,9 +10,19 @@ const listProducts = () => async (dispatch) => {
     const { data } = await axios.get("/api/products");
     dispatch({ type: PRODUCT_LIST_SUCCESS, payload: data });
 }
-catch(error) {
+catch (error) {
     dispatch({ type: PRODUCT_LIST_FAIL, payload: error.message });
 }
 }
 
-export { listProducts }
+const detailsProduct = (productId) => async (dispatch) => {
+    try {
+        dispatch({type: PRODUCT_DETAILS_REQUEST, payload: productId});
+        const {data} = await axios.get("/api/products/" + productId);
+        dispatch({ type: PRODUCT_DETAILS_SUCCESS, payload: data });
+    } catch (error) {
+        dispatch({type: PRODUCT_DETAILS_FAIL, payload: error.message});
+    }
+}
+
+export { listProducts, detailsProduct }

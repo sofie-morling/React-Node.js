98f7dada84bf412206a76e85286dcfa1cfdd2b27
Sofie Morling <you@example.com>
2021-05-24 10:33:09 +0200

    more code and css to aboutUs screen

--- amazon/frontend/src/Screens/AboutUs.js
+++ amazon/frontend/src/Screens/AboutUs.js
@@ -7,11 +7,20 @@ const AboutUs = () => {
             <div>
                 <img className="pic-10" src="/images/Bild10.jpg" alt="barOnOcean" />
             </div>
+            <h1 className="aboutUs-headText">
+                About Us
+            </h1>
+            <div className="text-border">
             <div className="text-one">
                 <p>
                 Sed ut perspiciatis unde omnis iste natus error sit voluptatem 
-                accusantium doloremque laudantium, totam rem aperiam, eaque ipsa 
-                quae ab illo inventore veritatis et quasi architecto beatae vitae 
+                accusantium doloremque laudantium, totam rem aperiam, eaque ipsa
+                quae ab illo inventore veritatis et quasi architecto beatae vitae
+                <p>
+                dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit 
+                aspernatur aut odit aut fugit, sed quia consequuntur magni dolores 
+                eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est
+                </p> 
                 dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit 
                 aspernatur aut odit aut fugit, sed quia consequuntur magni dolores 
                 eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est,
@@ -25,10 +34,21 @@ const AboutUs = () => {
                 nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut 
                 aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit 
                 qui in ea voluptate velit esse quam nihil molestiae consequatur, vel 
-                illum qui dolorem eum fugiat quo voluptas nulla pariatur. 
+                illum qui dolorem eum fugiat quo voluptas nulla pariatur.
+                aspernatur aut odit aut fugit, sed quia consequuntur magni dolores 
+                eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est. 
                 </p>
+                <br></br>
+            <br></br>
+            <br></br>
             </div>
-
+        </div>
+        <img className="pic-11" src="/images/Bild11.jpg" alt="sunset bar" />
+        <br></br>
+        <br></br>
+        <br></br>
+        <br></br>
+        <br></br>
         </div>
     )
 }

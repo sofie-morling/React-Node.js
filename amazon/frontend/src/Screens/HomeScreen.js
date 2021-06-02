b591d4b5cf310bb4e46338eb384c4b6ee10a5f3d
Sofie Morling <you@example.com>
2021-06-02 15:15:13 +0200

    code and css to frontend

--- amazon/frontend/src/Screens/HomeScreen.js
+++ amazon/frontend/src/Screens/HomeScreen.js
@@ -18,15 +18,34 @@ const HomeScreen = () => {
              </div>
         
             <div>
+              <div className="containPic-2">
                  <img className="pic-2" src="/images/Bild4.jpg" alt="bluedrink" />
-
+                  <div className="middle">
+                    <div className="textPic-2">Shop Now!</div>
+                  </div>
+                  </div>
                  <img className="pic-7" src="/images/Bild7.jpg" alt="fastDilevry"/>
             </div>
             </div>
             <div className="containBodyTwo">
-                <img className="pic-8" src="/images/Bild8.jpg" alt="beachDrink" /> 
+              <div className="containPic-8">
+                <img className="pic-8" src="/images/Bild8.jpg" alt="beachDrink" />
+                <div className="middle-8">
+                  <div className="textpic-8">Fresh Cocktails</div>
+                </div>
+                </div>
+                <div className="containPic-9">
                 <img className="pic-9" src="/images/Bild9.jpg" alt="blueCoctail" />
+                <div className="middle-9">
+                  <div className="textpic-9">Many flavours</div>
+                </div>
+                </div>
+                <div className="containPic-6">
                 <img className="pic-6" src="/images/Bild6.jpg" alt="Tropics" />
+                <div className="middle-6">
+                  <div className="textPic-6">Happy Hours!</div>
+                </div>
+                </div>
             </div>
         
                  <br></br>
